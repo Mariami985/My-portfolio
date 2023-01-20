00c6062d0f2d4d1792034b87992ce7e11eb6e4c0
Mariami985 <mariami.bregvadze17b@geolab.edu.ge>
2023-01-21 01:31:52 +0400

    add home and about section

---
+++ app.js
@@ -0,0 +1,9 @@
+let navBar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick  = () => {
+     navBar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+     navBar.classList.remove('active');
+}
