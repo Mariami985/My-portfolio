c98c253ff3874814250459e9c70a30ec84242692
Mariami985 <mariami.bregvadze17b@geolab.edu.ge>
2023-01-24 17:59:56 +0400

    create contact and footer section

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 let navBar = document.querySelector('.header .navbar');
+let contactInfo = document.querySelector('.contact-info')
+
 
 document.querySelector('#menu-btn').onclick  = () => {
      navBar.classList.toggle('active');
 }
 
+document.querySelector('#contact').onclick = () => {
+     contactInfo.classList.toggle('active');
+};
+document.querySelector('#close-contact-info').onclick = () => {
+     contactInfo.classList.remove('active');
+};
+
 window.onscroll = () => {
      navBar.classList.remove('active');
+     contactInfo.classList.remove('active');
+
 }
